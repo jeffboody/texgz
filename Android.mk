a4488fef4ff8ebd2e5bb84c48c5c35ba0aedef89
Jeff Boody <jeffboody@gmail.com>
2012-05-01 00:23:20 -0600

    add Android.mk

---
+++ Android.mk
@@ -0,0 +1,11 @@
+# don't include LOCAL_PATH for submodules
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := texgz
+LOCAL_CFLAGS    := -Wall
+LOCAL_SRC_FILES := texgz/texgz_tex.c
+
+LOCAL_LDLIBS    := -Llibs/armeabi \
+                   -llog -lz
+
+include $(BUILD_SHARED_LIBRARY)
