2365a50388a18c1d4d42cebe0a54b2116eb025f6
Jeff Boody <jeffboody@gmail.com>
2013-06-03 01:19:47 +0100

    add tile2coord and coord2tile mgm examples

--- mgm/example.lua
+++ mgm/example.lua
@@ -1,5 +1,7 @@
+#!/usr/bin/lua
+
 -- Example to convert latitude/longitude
--- to tile/subtile indicies/coords
+-- to/from tile/subtile indicies/coords
 -- for MGM map filenames
 --
 -- Based loosely on this description from JCamp...@gmail.com
@@ -8,41 +10,75 @@
 -- For a good description of tiles and zoom levels
 -- http://www.mapbox.com/developers/guide/
 --
+-- For a description of the Mercator Projection
+-- http://en.m.wikipedia.org/wiki/Mercator_projection
+--
 -- Note that math.log is ln
 
--- home
-Lat = 40.061295
-Lng = -105.214552
+function coord2tile(Zoom, Lat, Lon)
+	local PI         = 3.141592654
+	local RadLat     = Lat*(PI/180)
+	local RadLon     = Lon*(PI/180)
+	local MercX      = RadLon
+	local MercY      = math.log(math.tan(RadLat) + 1/math.cos(RadLat))
+	local CartX      = MercX + PI
+	local CartY      = PI - MercY
+	local WorldU     = CartX/(2*PI)
+	local WorldV     = CartY/(2*PI)
+	local NumSubTile = 8
+	local TileX      = WorldU*(2^Zoom)/NumSubTile
+	local TileY      = WorldV*(2^Zoom)/NumSubTile
+	local TileIX     = math.floor(TileX)
+	local TileIY     = math.floor(TileY)
+	local SubTileX   = NumSubTile*(TileX - TileIX)
+	local SubTileY   = NumSubTile*(TileY - TileIY)
+	local SubTileIX  = math.floor(SubTileX)
+	local SubTileIY  = math.floor(SubTileY)
+	local SubTileU   = SubTileX - SubTileIX
+	local SubTileV   = SubTileY - SubTileIY
 
--- compute tile/subtile indices/coords
-Zoom       = 14
-PI         = 3.141592654
-RadLat     = Lat*(PI/180)
-RadLng     = Lng*(PI/180)
-MercX      = RadLng
-MercY      = math.log(math.tan(RadLat) + 1/math.cos(RadLat))
-CartX      = MercX + PI
-CartY      = PI - MercY
-WorldU     = CartX/(2*PI)
-WorldV     = CartY/(2*PI)
-NumSubTile = 8
-TileX      = WorldU*(2^Zoom)/NumSubTile
-TileY      = WorldV*(2^Zoom)/NumSubTile
-TileIX     = math.floor(TileX)
-TileIY     = math.floor(TileY)
-SubTileX   = NumSubTile*(TileX - TileIX)
-SubTileY   = NumSubTile*(TileY - TileIY)
-SubTileIX  = math.floor(SubTileX)
-SubTileIY  = math.floor(SubTileY)
-SubTileU   = SubTileX - SubTileIX
-SubTileV   = SubTileY - SubTileIY
+	-- sumarize stats
+	print("-- coord2tile --")
+	print("WorldU    = " .. WorldU)
+	print("WorldV    = " .. WorldV)
+	print("TileIX    = " .. TileIX)
+	print("TileIY    = " .. TileIY)
+	print("SubTileIX = " .. SubTileIX)
+	print("SubTileIY = " .. SubTileIY)
+	print("SubTileU  = " .. SubTileU)
+	print("SubTileV  = " .. SubTileV)
 
--- sumarize stats
-print("WorldU    = " .. WorldU)
-print("WorldV    = " .. WorldV)
-print("TileIX    = " .. TileIX)
-print("TileIY    = " .. TileIY)
-print("SubTileIX = " .. SubTileIX)
-print("SubTileIY = " .. SubTileIY)
-print("SubTileU  = " .. SubTileU)
-print("SubTileV  = " .. SubTileV)
+	return { x=TileX, y=TileY }
+end
+
+function tile2coord(Zoom, TileX, TileY)
+	local PI         = 3.141592654
+	local NumSubTile = 8
+	local WorldU     = NumSubTile*TileX/(2^Zoom)
+	local WorldV     = NumSubTile*TileY/(2^Zoom)
+	local CartX      = 2*PI*WorldU
+	local CartY      = 2*PI*WorldV
+	local MercX      = CartX - PI
+	local MercY      = PI - CartY
+	local RadLon     = MercX
+	local RadLat     = 2*math.atan(math.exp(MercY)) - PI/2
+	local Lat        = RadLat/(PI/180)
+	local Lon        = RadLon/(PI/180)
+
+	print("-- tile2coord --")
+	print("WorldU = " .. WorldU)
+	print("WorldV = " .. WorldV)
+	print("Lat    = " .. Lat)
+	print("Lon    = " .. Lon)
+
+	return { lat=Lat, lon=Lon }
+end
+
+home = { lat=40.061295, lon=-105.214552 }
+
+print("-- home --")
+print("Lat = " .. home.lat)
+print("Lon = " .. home.lon)
+
+tile  = coord2tile(14, home.lat, home.lon)
+coord = tile2coord(14, tile.x, tile.y)
